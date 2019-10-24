b1bf85cad72a3c8f92c581a1521bbb109113d532
Diego Doná <diedona@gmail.com>
2019-10-24 12:50:07 -0300

    subscription ctor + student ireadonlycollection

--- DDona.DDD.PaymentContext.Domain/Entities/Student.cs
+++ DDona.DDD.PaymentContext.Domain/Entities/Student.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DDona.DDD.PaymentContext.Domain.Entities
 {
     public class Student
     {
+        private IList<Subscription> _subscriptions;
+
         public Student(string firstName, string lastName, string document, string email)
         {
             FirstName = firstName;
             LastName = lastName;
             Document = document;
             Email = email;
+            _subscriptions = new List<Subscription>();
         }
 
         public string FirstName { get; private set; }
@@ -19,6 +23,27 @@ namespace DDona.DDD.PaymentContext.Domain.Entities
         public string Document { get; private set; }
         public string Email { get; private set; }
         public string Address { get; private set; }
-        public IList<Subscription> Subscriptions { get; set; }
+        public IReadOnlyCollection<Subscription> Subscriptions
+        {
+            get { return _subscriptions.ToArray(); }
+        }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            // cant add inactive subscription
+            if (!subscription.Active)
+            {
+                throw new Exception("Subscription is inactive!");
+            }
+
+            // cancel all current subscriptions
+            foreach (var sub in this.Subscriptions)
+            {
+                sub.SetInactive();
+            }
+
+            // add current active
+            _subscriptions.Add(subscription);
+        }
     }
 }
--- DDona.DDD.PaymentContext.Domain/Entities/Subscription.cs
+++ DDona.DDD.PaymentContext.Domain/Entities/Subscription.cs
@@ -6,9 +6,21 @@ namespace DDona.DDD.PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public DateTime? ExpireDate { get; set; }
-        public bool Active { get; set; }
+        public Subscription(DateTime createDate, DateTime lastUpdateDate, bool active)
+        {
+            CreateDate = createDate;
+            LastUpdateDate = lastUpdateDate;
+            Active = active;
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+
+        public void SetInactive()
+        {
+            this.Active = false;
+        }
     }
 }
