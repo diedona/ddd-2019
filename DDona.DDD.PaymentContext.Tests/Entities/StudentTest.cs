0a6a01c67eb79a8a7be902a5902cc2e445aa6e7a
Diego Doná <diedona@gmail.com>
2019-10-24 12:36:44 -0300

    constructor logic

---
+++ DDona.DDD.PaymentContext.Tests/Entities/StudentTest.cs
@@ -0,0 +1,15 @@
+﻿using DDona.DDD.PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.DDD.PaymentContext.Tests.Entities
+{
+    public class StudentTest
+    {
+        public void TestStudent()
+        {
+            var student = new Student("Diego", "Doná", "123123123", "diedona@gmail.com");
+        }
+    }
+}
