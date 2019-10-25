b91e8e066fc5392540fd1bcd2ca0b6f73bc49c70
Diego Doná <diedona@gmail.com>
2019-10-25 16:59:11 -0300

    refactor tests, cnpj, cpf

--- DDona.DDD.PaymentContext.Tests/ValueObjects/DocumentTest.cs
+++ DDona.DDD.PaymentContext.Tests/ValueObjects/DocumentTest.cs
@@ -9,24 +9,47 @@ namespace DDona.DDD.PaymentContext.Tests.ValueObjects
 {
     public class DocumentTest
     {
-        [Fact]
-        public void Should_Fail_Invalid_Length_CPF()
+        [Theory]
+        [InlineData("123")]
+        [InlineData("123111111111111111111111")]
+        [InlineData("12222222222222222222222223")]
+        [InlineData("12333333")]
+        [InlineData("11520020073")]
+        public void Should_Fail_Invalid_CPF(string cpf)
         {
-            Document document = new Document("123", EDocumentType.CPF);
+            Document document = new Document(cpf, EDocumentType.CPF);
             Assert.False(document.Valid);
         }
 
-        [Fact]
-        public void Should_Fail_Invalid_Digits_CPF()
+        [Theory]
+        [InlineData("77198440096")]
+        [InlineData("33686373041")]
+        [InlineData("43889411070")]
+        public void Should_Success_Valid_CPF(string cpf)
         {
-            Document document = new Document("11520020073", EDocumentType.CPF);
+            Document document = new Document(cpf, EDocumentType.CPF);
+            Assert.True(document.Valid);
+        }
+
+        [Theory]
+        [InlineData("123")]
+        [InlineData("123111111111111111111111")]
+        [InlineData("12222222222222222222222223")]
+        [InlineData("12333333")]
+        [InlineData("40124269000108")]
+        public void Should_Fail_Invalid_CNPJ(string cnpj)
+        {
+            Document document = new Document(cnpj, EDocumentType.CNPJ);
             Assert.False(document.Valid);
         }
 
-        [Fact]
-        public void Should_Success_Valid_CPF()
+        [Theory]
+        [InlineData("40124269000102")]
+        [InlineData("83087091000171")]
+        [InlineData("19859091000143")]
+        public void Should_Success_Valid_CNPJ(string cnpj)
         {
-            Document document = new Document("11520020074", EDocumentType.CPF);
+            Document document = new Document(cnpj, EDocumentType.CNPJ);
             Assert.True(document.Valid);
         }
     }
