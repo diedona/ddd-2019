f7f4d7e9dc7bb8696e3c5785c36fe5ce74b4d862
Diego Doná <diedona@gmail.com>
2019-10-25 16:44:27 -0300

    document (cpf) validation

---
+++ DDona.DDD.PaymentContext.Tests/ValueObjects/DocumentTest.cs
@@ -0,0 +1,33 @@
+﻿using DDona.DDD.PaymentContext.Domain.Enums;
+using DDona.DDD.PaymentContext.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DDona.DDD.PaymentContext.Tests.ValueObjects
+{
+    public class DocumentTest
+    {
+        [Fact]
+        public void Should_Fail_Invalid_Length_CPF()
+        {
+            Document document = new Document("123", EDocumentType.CPF);
+            Assert.False(document.Valid);
+        }
+
+        [Fact]
+        public void Should_Fail_Invalid_Digits_CPF()
+        {
+            Document document = new Document("11520020073", EDocumentType.CPF);
+            Assert.False(document.Valid);
+        }
+
+        [Fact]
+        public void Should_Success_Valid_CPF()
+        {
+            Document document = new Document("11520020074", EDocumentType.CPF);
+            Assert.True(document.Valid);
+        }
+    }
+}
