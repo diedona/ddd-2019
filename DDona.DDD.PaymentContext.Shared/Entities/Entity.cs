06ade019270a2c47fde8d60bfc0fe9bd2c8bad0f
Diego Doná <diedona@gmail.com>
2019-10-24 21:33:11 -0300

    fluent for entity

--- DDona.DDD.PaymentContext.Shared/Entities/Entity.cs
+++ DDona.DDD.PaymentContext.Shared/Entities/Entity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,5 +14,14 @@ namespace DDona.DDD.PaymentContext.Shared.Entities
         }
 
         public Guid Id { get; private set; }
+        public bool Valid { get; private set; }
+        public bool Invalid => !Valid;
+        public ValidationResult ValidationResult { get; private set; }
+
+        public bool Validate<T>(T model, AbstractValidator<T> validator) where T : Entity
+        {
+            ValidationResult = validator.Validate(model);
+            return Valid = ValidationResult.IsValid;
+        }
     }
 }
