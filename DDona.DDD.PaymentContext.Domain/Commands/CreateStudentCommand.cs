61eb15245a050b15b117a39867612ccf08a80feb
Diego Doná <diedona@gmail.com>
2019-10-26 01:23:24 -0300

    command

---
+++ DDona.DDD.PaymentContext.Domain/Commands/CreateStudentCommand.cs
@@ -0,0 +1,31 @@
+﻿using DDona.DDD.PaymentContext.Domain.Enums;
+using DDona.DDD.PaymentContext.Shared.Commands;
+using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.DDD.PaymentContext.Domain.Commands
+{
+    public class CreateStudentCommand : Notifiable, ICommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string DocumentNumber { get; set; }
+        public EDocumentType DocumentType { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public bool Validate()
+        {
+            AddNotifications(new Contract().Requires()
+                .IsNotNullOrEmpty(FirstName, "FirstName", "Invalid first name")
+                .IsNotNullOrEmpty(FirstName, "FirstName", "Invalid first name")
+            );
+
+            return this.Valid;
+        }
+    }
+}
---
+++ DDona.DDD.PaymentContext.Shared/Commands/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.DDD.PaymentContext.Shared.Commands
+{
+    public interface ICommand
+    {
+        bool Validate();
+    }
+}
